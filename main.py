52d5c9812d4ac6b7856b7fc06f2e5e9150f25601
Clement Joudet <clement.joudet@gmail.com>
2019-02-28 20:23:15 +0100

    Dumb pairing in results

--- main.py
+++ main.py
@@ -17,11 +17,42 @@ def my_solution(input_data):
     solution = []
     verticals = [[i, lines[i][1]] for i in range(len(lines)) if lines[i][0] == 1]
     verticals = sort_verticals(verticals)
-    result = [[i] for i in range(len(lines)) if lines[i][0] == 0] + verticals
-    return result
+    a = [[i] for i in range(len(lines)) if lines[i][0] == 0] + verticals
+    return construct_results(a, lines)
+
+def get_keywords(photo, lines):
+    if len(photo) == 2:
+        return lines[photo[0]][1].union(lines[photo[1]][1])
+    return lines[photo[0]][1]
+
+def pair_score(photo1, photo2, lines):
+    keywords1 = get_keywords(photo1, lines)
+    keywords2 = get_keywords(photo2, lines)
+    num_inter = len(keywords1.intersection(keywords2))
+    num_1_minus_2 = len(keywords1.difference(keywords2))
+    num_2_minus_1 = len(keywords2.difference(keywords1))
+    return min(num_inter, num_1_minus_2, num_2_minus_1)
+
+def construct_results(photos, lines):
+    results = []
+    while len(photos) > 1:
+        element = photos.pop()
+        index = find_index_best_pair(element, photos, lines)
+        results += [element, photos[index]]
+        del photos[index]
+    return results
+
+def find_index_best_pair(photo, photos, lines):
+    lim = 50
+    if len(photos) < lim:
+        return 0
+    for i in range(lim):
+        if pair_score(photo, photos[i], lines) > 0:
+            return i
+    return 0
 
 def find_index_best(tags, verticals):
-    lim = 100
+    lim = 300
     if len(verticals) < lim:
         return 0
     for i in range(lim):
@@ -31,7 +62,7 @@ def find_index_best(tags, verticals):
 
 def sort_verticals(verticals):
     couples = []
-    while len(verticals):
+    while len(verticals) > 2:
         element = verticals.pop()
         index = find_index_best(element[1], verticals)
         couple = verticals[index][0]
