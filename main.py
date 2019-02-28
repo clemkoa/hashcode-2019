73391fe37f0bccad32f768639eaa41d69e550707
Clement Joudet <clement.joudet@gmail.com>
2019-02-28 18:07:42 +0100

    oops

--- main.py
+++ main.py
@@ -3,30 +3,6 @@ import numpy as np
 
 import utils
 
-<<<<<<< HEAD
-def read(INPUT_FOLDER, file_name):
-    path = os.path.join(INPUT_FOLDER, file_name)
-    with open(path, 'r') as f:
-        R, C, L, H = map(int, next(f).split())
-
-        def parse_line(line):
-            return [0 if c is 'M' else 1 for c in line]
-
-        lines = np.array([parse_line(line) for line in f])
-        return R, C, L, H, lines
-
-def write(path, solution):
-    folder = os.path.dirname(path)
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-
-    with open(path, 'w') as f:
-        f.write(str(len(solution)) + '\n')
-        for rect in solution:
-            f.write(' '.join(map(str, rect)) + '\n')
-
-=======
->>>>>>> 67fa4b6efb7734721169988507a5a129257ce7b5
 def is_slice_valid(pizza, xmin, ymin, xmax, ymax, R, C, L, H):
     slice = pizza[xmin:xmax + 1, ymin:ymax + 1]
     if xmax >= R - 1: return False
@@ -47,17 +23,4 @@ def my_solution(input_data):
     return solution
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    INPUT_FOLDER = 'input'
-    OUTPUT_FOLDER = 'output'
-    utils.zip_code()
-    files = sorted(os.listdir(INPUT_FOLDER))
-    for file in files:
-        print(file)
-        R, C, L, H, lines = read(INPUT_FOLDER, file)
-        solution = build_solution(R, C, L, H, lines)
-        print(solution)
-        write(os.path.join(OUTPUT_FOLDER, file), solution)
-=======
     utils.run_solution(my_solution, local_only=True)
->>>>>>> 67fa4b6efb7734721169988507a5a129257ce7b5
